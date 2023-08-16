413adbb5b4031630fe263066b7c305515d024060
Martin Bischof <>
2023-08-16 21:40:49 +0200

    with animations

--- scroll.js
+++ scroll.js
@@ -1,5 +1,8 @@
-// this is called everytime the visibility of a element changes
-const observer = new IntersectionObserver((entries) => {
+const options = {
+    // threshold: 0.5
+}
+
+const hiddenIconsObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
@@ -7,8 +10,24 @@ const observer = new IntersectionObserver((entries) => {
             entry.target.classList.remove('show');
         }
     });
-});
+}, options);
+
+// this is called everytime the visibility of a element changes
+const hiddenCardsObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry.target, entry.intersectionRatio)
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, options);
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach(el => observer.observe(el));
+const hiddenIconElements = document.querySelectorAll('.hidden-icon');
+const hiddenCardElements = document.querySelectorAll('.hidden-card');
+
+hiddenIconElements.forEach(el => hiddenIconsObserver.observe(el));
+hiddenCardElements.forEach(el => hiddenCardsObserver.observe(el));

e8435521a65dc68ab02243279949b89421ddd07b
Martin Bischof <>
2023-08-17 11:00:28 +0200

    [FEATURE] - added card-animation

--- scroll.js
+++ scroll.js
@@ -1,29 +1,21 @@
-const options = {
-    // threshold: 0.5
-}
-
 const hiddenIconsObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+        entry.target.classList.toggle('show', entry.isIntersecting);
     });
-}, options);
+}, {threshold: 1});
 
 // this is called everytime the visibility of a element changes
 const hiddenCardsObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry.target, entry.intersectionRatio)
+        // console.log(entry.target, entry.intersectionRatio)
 
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+        entry.target.classList.toggle('visible-card', entry.isIntersecting);
+
+        // if (entry.isIntersecting) {
+        //     hiddenCardsObserver.unobserve(entry.target);
+        // }
     });
-}, options);
+}, {threshold: 0.5});
 
 
 const hiddenIconElements = document.querySelectorAll('.hidden-icon');

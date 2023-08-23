fda89366a2d4a11c4a68a820ea969db5c5950937
Martin Bischof <>
2023-08-23 17:10:21 +0200

    [FEATURE] - fixed animations on mobile devices

--- public/scroll.js
+++ public/scroll.js
@@ -2,24 +2,32 @@ const hiddenIconsObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         entry.target.classList.toggle('show', entry.isIntersecting);
     });
-}, {threshold: 1});
+}, {threshold: 0.8});
 
-// this is called everytime the visibility of a element changes
-const hiddenCardsObserver = new IntersectionObserver((entries) => {
+const cardsAppearingObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        // console.log(entry.target, entry.intersectionRatio)
+        // console.log({ entry: entry.target, ratio: entry.intersectionRatio });
 
-        entry.target.classList.toggle('visible-card', entry.isIntersecting);
-
-        // if (entry.isIntersecting) {
-        //     hiddenCardsObserver.unobserve(entry.target);
-        // }
+        if (entry.isIntersecting && !entry.target.classList.contains("visible-card")) {
+            entry.target.classList.add("visible-card");
+        }
     });
 }, {threshold: 0.5});
+
+const cardsDisappearingObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log({ event: 'in disappearing', entry: entry.target, ratio: entry.intersectionRatio });
+
+        if (!entry.isIntersecting && entry.target.classList.contains("visible-card")) {
+            entry.target.classList.remove("visible-card");
+        }
+    });
+}, {threshold: 0.4, rootMargin: '5%'});
 
 
 const hiddenIconElements = document.querySelectorAll('.hidden-icon');
 const hiddenCardElements = document.querySelectorAll('.hidden-card');
 
 hiddenIconElements.forEach(el => hiddenIconsObserver.observe(el));
-hiddenCardElements.forEach(el => hiddenCardsObserver.observe(el));
+hiddenCardElements.forEach(el => cardsAppearingObserver.observe(el));
+hiddenCardElements.forEach(el => cardsDisappearingObserver.observe(el));
